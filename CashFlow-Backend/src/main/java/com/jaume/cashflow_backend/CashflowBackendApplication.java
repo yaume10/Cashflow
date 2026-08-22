82209e529ddd2be307b9d82fb30100cf1a9fe343
Jaume <jgs@gmail.com>
2026-08-22 17:07:08 +0200

    Reestructuración: Separación de Frontend y Backend

---
+++ CashFlow-Backend/src/main/java/com/jaume/cashflow_backend/CashflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jaume.cashflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowBackendApplication.class, args);
+	}
+
+}
---
+++ CashFlow-Backend/src/test/java/com/jaume/cashflow_backend/CashflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaume.cashflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
